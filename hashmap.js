56352f9151c77a500fdcf06d903d3de89f576ea1
DocQuantic <35266773+DocQuantic@users.noreply.github.com>
2025-07-25 21:39:04 +0200

    Add main functions for hashmap handling

--- hashmap.js
+++ hashmap.js
@@ -1,19 +1,9 @@
-import {default as LinkedList} from "./linkedList.js"
-
 export default class HashMap {
     static loadFactor = 0.5
     constructor(){
         this.capacity = 24
-        this.buckets = this.createEmptyBuckets(this.capacity)
-    }
-
-    createEmptyBuckets(size){
-        let tmp = []
-        for(let i=0; i<=this.capacity; i++){
-            tmp.push(new LinkedList())
-        }
-
-        return tmp
+        this.load = 0
+        this.buckets = new Array(this.capacity)
     }
 
     hash(key) {
@@ -26,4 +16,85 @@ export default class HashMap {
 
         return hashCode ;
     } 
+
+    set(key, value){
+        const keyHash = this.hash(key)
+
+        if (keyHash < 0 || keyHash >= this.capacity) {
+            throw new Error("Trying to access index out of bounds");
+        }
+
+        this.buckets[keyHash] = {keyHash, value}
+        /*Check load and grow if needed*/
+    }
+
+    get(key){
+        const keyHash = this.hash(key)
+        const value = this.buckets[keyHash].value
+        if(value === undefined) return null
+        return value
+    }
+
+    has(key){
+        if(this.get(key) === null) return false
+        return true;
+    }
+
+    remove(key){
+        if(this.has(key)){
+            const keyHash = this.hash(key)
+            this.buckets[keyHash] = undefined
+            return true
+        } else {
+            return false
+        }
+    }
+
+    length(){
+        let count = 0
+        for(b in this.buckets){
+            if(b.key != undefined){
+                count++
+            }
+        }
+
+        return count
+    }
+
+    clear(){
+        this.buckets = new Array(this.capacity)
+    }
+
+    keys(){
+        let keys = []
+        for(b in this.buckets){
+            if(b.key != undefined){
+                keys.push(b.key)
+            }
+        }
+
+        return keys
+    }
+
+    values(){
+        let values = []
+        for(b in this.buckets){
+            if(b.key != undefined){
+                values.push(b.value)
+            }
+        }
+        
+        return values
+    }
+
+    entries(){
+        let entries = []
+        for(b in this.buckets){
+            if(b.key != undefined){
+                entries.push([b.key, b.value])
+            }
+        }
+        
+        return entries
+    }
 }
